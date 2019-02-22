f64c0a5ddef1d00ddea3740a48171aaa2788edb9
Andrew Laskey <andrew.laskey@gmail.com>
2019-02-21 18:27:26 -0800

    Add contact form

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">About</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
---
+++ pages/contact.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Contact
+      </h1>
+      <form accept-charset="UTF-8" action="https://usebasin.com/f/6a4f72ca11e4" method="POST">
+        <div class="field">
+          <label class="label">
+            Name
+          </label>
+          <div class="control">
+            <input id="name" class="input" type="text" name="name" required>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">
+            Email
+          </label>
+          <div class="control">
+            <input id="email" class="input" type="email" name="email" required>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">
+            Subject
+          </label>
+          <div class="control">
+            <div class="select">
+              <select id="subject" name="subject">
+                <option>Edit my listing</option>
+                <option>Remove my listing</option>
+                <option>Report a listing</option>
+                <option>Payments</option>
+                <option>General question or comment</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <label class="label">
+          Comment
+        </label>
+        <div class="field">
+          <div class="control">
+            <textarea id="comment" class="textarea" name="comment"></textarea>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary" type="submit">
+              Contact Us
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
