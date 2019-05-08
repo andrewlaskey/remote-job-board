069b6ec0f5af9ff6067c91f3d311786172794a75
Andrew Laskey <andrew.laskey@gmail.com>
2019-05-07 23:12:04 -0700

    add stripe to .env

--- plugins/vue-stripe.js
+++ plugins/vue-stripe.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
 import VueStripeCheckout from 'vue-stripe-checkout';
-import { publishableKey as stripePubKey } from './../stripe.config.js';
 
-Vue.use(VueStripeCheckout, stripePubKey);
+Vue.use(VueStripeCheckout, process.env.STRIPE_PUBLISHABLE_KEY);
